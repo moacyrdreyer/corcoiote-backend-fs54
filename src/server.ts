1d4df63e0bf304e4d8cfc8e66e8e4f06485d9787
Moacyr Dreyer <moadreyer01@gmail.com>
2026-07-17 19:11:48 -0300

    feat: implementar servidor basico

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-import http from 'node:http';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/customers', (request, response) => {
+    const customers = [{
+        name: 'Moacyr Dreyer',
+        status: true
+    }, {
+        name: 'Maria Clara',
+        status: false
+    }, {
+        name: 'João Pedro',
+        status: true
+    }];
+    
+    response.status(200).json(customers);
+});
+
+app.listen(3000);
