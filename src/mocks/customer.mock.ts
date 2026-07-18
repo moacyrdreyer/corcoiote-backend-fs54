e3b3364104ea289088c280c33c22e83c3adf0188
Moacyr Dreyer <moadreyer01@gmail.com>
2026-07-17 21:40:03 -0300

    refact: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,17 @@
+import type { Customer } from '../types.ts';
+
+const customers: Customer[] = [{
+        id: 1,
+        name: 'Moacyr Dreyer',
+        status: true
+    }, {
+        id: 2,
+        name: 'Maria Clara',
+        status: false
+    }, {
+        id: 3,
+        name: 'João Pedro',
+        status: true
+    }];
+
+export default customers;
